a49b8b75e53ca6fbca642131a5ba3f2f6f9462d7
Nikita Tokarchuk <nikita@tokarch.uk>
2021-12-03 04:25:18 +0100

    Add webpack entrypoint script

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
+const path = require('path');
 
 module.exports = {
+  entry: './web/index.js',
+  output: {
+    clean: true,
+    filename: 'js-bin/app.js',
+    path: path.resolve(__dirname, 'out'),
+  },
   module: {
   },
 };
