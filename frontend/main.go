c20306c118d90bf18ee186deac79413218f3201f
Nikita Tokarchuk <nikita@tokarch.uk>
2021-12-03 02:17:27 +0100

    Unmarshal config values into app struct

--- frontend/main.go
+++ frontend/main.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
+
+	"code.tokarch.uk/mainnika/nikita-tokarch-uk/frontend/config"
 )
 
 var Version = "nightly"
@@ -21,5 +23,11 @@ func main() {
 		logrus.Debugf("Skip invalid config file %s, %v", viper.ConfigFileUsed(), err)
 	}
 
+	config := &config.Config{}
+	if err := viper.Unmarshal(config); err != nil {
+		logrus.Warnf("Cannot unmarshal config, %v", err)
+	}
+
 	logrus.Infof("Version: %s", Version)
+	logrus.Debugf("Conf: %#v", config)
 }
