47029b5a4c7f4ea75482f292dbe227ca6f5b9ab7
Nikita Tokarchuk <nikita@tokarch.uk>
2021-12-03 04:04:46 +0100

    Load index page data as the content

--- frontend/content/index.go
+++ frontend/content/index.go
@@ -1,6 +1,13 @@
 package content
 
+import (
+	"code.tokarch.uk/mainnika/nikita-tokarch-uk/frontend/ghost"
+)
+
 // Index content data
 type Index struct {
 	_ interface{} `template:"index.go.tmpl"`
+	ghost.Meta
+	Pinned []ghost.Post
+	Posts  []ghost.Post
 }
--- frontend/renderer/index.go
+++ frontend/renderer/index.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"code.tokarch.uk/mainnika/nikita-tokarch-uk/frontend/content"
+	"code.tokarch.uk/mainnika/nikita-tokarch-uk/frontend/ghost"
 	"code.tokarch.uk/mainnika/nikita-tokarch-uk/frontend/templates"
 	routing "github.com/jackwhelpton/fasthttp-routing/v2"
 )
@@ -19,7 +20,23 @@ func (r *Renderer) rootRedirect(c *routing.Context) (err error) {
 // index handler renders index data
 func (r *Renderer) index(c *routing.Context) (err error) {
 
+	pinnedPageSlug := r.ContentConfig.Pinned
+	postsPerPage := r.ContentConfig.PostsPerPage
+
+	pinnedPages, err := r.GhostClient.GetPageBySlug(pinnedPageSlug)
+	if err != nil {
+		return
+	}
+
+	latestPosts, err := r.GhostClient.GetPosts(ghost.QueryLimit(postsPerPage))
+	if err != nil {
+		return
+	}
+
 	indexContent := content.Index{
+		Pinned: pinnedPages.Pages,
+		Meta:   latestPosts.Meta,
+		Posts:  latestPosts.Posts,
 	}
 
 	return c.Write(indexContent)
