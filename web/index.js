f18298834684b47dec69dd8f6d231502bc903cf0
Nikita Tokarchuk <nikita@tokarch.uk>
2021-12-03 04:30:05 +0100

    Use google code prettifier

--- web/index.js
+++ web/index.js
@@ -1,4 +1,9 @@
 import "./font.css"
 import "./index.scss"
 
+import "code-prettify/styles/desert.css" 
 import "bootstrap-icons/font/bootstrap-icons.css"
+
+import "code-prettify"
+
+PR.prettyPrint();
