9b43855d077603f8ca09645de80507f68003a4d0
Nikita Tokarchuk <nikita@tokarch.uk>
2021-12-03 04:19:53 +0100

    Render pages pagination

--- frontend/templates/funcs.go
+++ frontend/templates/funcs.go
@@ -4,8 +4,17 @@ import "html/template"
 
 func UseFuncs() template.FuncMap {
 	return template.FuncMap{
+		"add": func(i int) int {
+			return i + 1
+		},
+		"sub": func(i int) int {
+			return i - 1
+		},
 		"getIndexURL": func() string {
 			return URLIndex
+		},
+		"getBlogURL": func() string {
+			return URLBlog
 		},
 	}
 }
