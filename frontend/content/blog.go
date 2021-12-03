7a694bcbe8d28b17283fe804773e8ca12d9526e1
Nikita Tokarchuk <nikita@tokarch.uk>
2021-12-03 04:05:29 +0100

    Load blog page data as the content

--- frontend/content/blog.go
+++ frontend/content/blog.go
@@ -1,6 +1,13 @@
 package content
 
+import (
+	"code.tokarch.uk/mainnika/nikita-tokarch-uk/frontend/ghost"
+)
+
 // Blog content data
 type Blog struct {
 	_ interface{} `template:"blog.go.tmpl"`
+	ghost.Meta
+	Pinned []ghost.Post
+	Posts  []ghost.Post
 }
