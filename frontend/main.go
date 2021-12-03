2e56c1daa64b63f6286f764bb3dc343cd01cea33
Nikita Tokarchuk <nikita@tokarch.uk>
2021-12-03 02:15:47 +0100

    Print app version if requested by flags

--- frontend/main.go
+++ frontend/main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
 
+var Version = "nightly"
+
 func main() {
+
+	if viper.GetBool("version") {
+		fmt.Print(Version)
+		return
+	}
 
 	viper.SetConfigFile(viper.GetString("config"))
 	if err := viper.ReadInConfig(); err != nil {
 		logrus.Debugf("Skip invalid config file %s, %v", viper.ConfigFileUsed(), err)
 	}
+
+	logrus.Infof("Version: %s", Version)
 }
