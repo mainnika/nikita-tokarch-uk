129a80904f89df6caadf907fb9179c84c825d24d
Nikita Tokarchuk <nikita@tokarch.uk>
2022-01-04 20:27:06 +0100

    Restructure scss files

--- web/index.js
+++ web/index.js
@@ -1,9 +1,8 @@
-import "./font.css"
-import "./index.scss"
-
 import "code-prettify/styles/desert.css" 
 import "bootstrap-icons/font/bootstrap-icons.css"
 
 import "code-prettify"
+
+import "./style/index.scss"
 
 PR.prettyPrint();
